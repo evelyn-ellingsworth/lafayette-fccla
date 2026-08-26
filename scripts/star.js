37d7ac63bf7a2036ae555dbfb3af3fddea061a0b
Evelyn Ellingsworth <evie.ellingsworth@gmail.com>
2026-08-26 14:26:47 -0400

    Create star.js

---
+++ scripts/star.js
@@ -0,0 +1,20 @@
+async function loadJSON() { 
+   // alert (siteBaseUrl);
+  try {
+    const response = await fetch("/scripts/event-titles.json");
+    const facts = await response.json();
+    var num = 0;
+    document.getElementById("event-text").textContent = facts.data[num];
+
+    setInterval(() => {
+        num++;
+        document.getElementById("fact-text").textContent = facts.data[num];
+    }, 7500); // 10,000 ms = 10 seconds
+
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+
+loadJSON();
