1417a322e0e3dbfdbbf9f81d8987216ba650e15c
Evelyn Ellingsworth <evie.ellingsworth@gmail.com>
2026-08-26 14:42:32 -0400

    Update star.js

--- scripts/star.js
+++ scripts/star.js
@@ -1,14 +1,14 @@
 async function loadJSON() { 
    // alert (siteBaseUrl);
   try {
-    const response = await fetch("/scripts/facts.json");
+    const response = await fetch("/scripts/event-titles.json");
     const facts = await response.json();
-    var num = Math.floor(Math.random() * facts.data.length);
-    document.getElementById("fact-text").textContent = facts.data[num];
+    var num = Math.floor(Math.random() * event-titles.data.length);
+    document.getElementById("fact-text").textContent = event-titles.data[num];
 
     setInterval(() => {
-        num = Math.floor(Math.random() * facts.data.length);
-        document.getElementById("fact-text").textContent = facts.data[num];
+        num = Math.floor(Math.random() * event-titles.data.length);
+        document.getElementById("fact-text").textContent = event-titles.data[num];
     }, 7500); // 10,000 ms = 10 seconds
 
   } catch (error) {
