c1e4f94d34c5f4c1623927b376373eea92f745f4
Evelyn Ellingsworth <evie.ellingsworth@gmail.com>
2026-09-10 14:49:47 -0400

    Update star.js

--- scripts/star.js
+++ scripts/star.js
@@ -7,7 +7,13 @@ async function loadJSON() {
     document.getElementById("event-text").textContent = eventTitles.data[num];
 
     setInterval(() => {
-        num = Math.floor(Math.random() * eventTitles.data.length);
+       if(num>eventTitles.data.length){
+          num=0;
+       }
+       else{
+          num++;
+       }
+        //num = Math.floor(Math.random() * eventTitles.data.length);
         document.getElementById("event-text").textContent = eventTitles.data[num];
     }, 7500); // 10,000 ms = 10 seconds
 
